3f8d379be196c0f273fbc118eae0cfdbc2744a11
Sebastian Vivas <svivas@modu.one>
2022-11-14 17:03:53 -0400

    validated that the two lovers wont cross the same path

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ class Graph:
     graph = nx.Graph()
     streets = [55, 54, 53, 52, 51, 50]
     avenues = [15, 14, 13, 12, 11, 10]
+    destinies = ['5410', '5014', '5012', '5213', '5414']
     path = []
+    javier_path = []
+    andreina_path = []
 
     def __init__(self):
         pass
@@ -55,20 +58,22 @@ class Graph:
     def show_graph(self):
         # Dibujar nodos en específico
         color_map = []
+        
         for node in self.graph.nodes():
-            if node == '5414' or node == '5213' or node == '5014' or node == '5411' or node == '5012':
-                if node == '5414':
-                    color_map.append(('#0ea2ea'))
-                if node == '5213':
-                    color_map.append(('#f1785e'))
-                if node == '5014':
-                    color_map.append(('#be76ce'))
-                if node == '5411':
-                    color_map.append(('#d88a02'))
-                if node == '5012':
-                    color_map.append(('#77a506'))
-            else:
-                color_map.append(('#a6cfcc'))
+            if(node not in self.javier_path and node not in self.andreina_path):
+                if node in self.destinies:
+                    if node == '5414':
+                        color_map.append(('#0ea2ea'))
+                    if node == '5213':
+                        color_map.append(('#f1785e'))
+                    if node == '5014':
+                        color_map.append(('#be76ce'))
+                    if node == '5411':
+                        color_map.append(('#d88a02'))
+                    if node == '5012':
+                        color_map.append(('#77a506'))
+                else:
+                    color_map.append(('#a6cfcc'))
 
         # Obtener la posición en pantalla de los nodos
         pos = nx.get_node_attributes(self.graph, 'pos')
@@ -106,84 +111,11 @@ class Graph:
             self.graph.edges[edge]['visitedByJavier'] = False
             self.graph.edges[edge]['visitedByAndreina'] = False
 
-
-#     def rundijkstra(self, initial_node, destination_node, person):
-#         # Set the initial node as visited
-#         self.graph.nodes[initial_node]['visited'] = True
-#         self.graph.nodes[initial_node]['visitedBy' + person] = True
-
-#         # Get the neighbors of the initial node
-#         neighbors = self.graph[initial_node]
-
-#         # For each neighbor, calculate the distance to the initial node and update the distance if it's lower
-#         for neighbor in neighbors:
-#             if self.graph.nodes[neighbor]['visited'] == False:
-#                 if self.graph.nodes[neighbor]['visitedBy' + person] == False:
-#                     self.graph.nodes[neighbor]['visitedBy' + person] = True
-#                     self.graph.nodes[neighbor][person + 'Distance'] = self.graph.nodes[initial_node][person +
-#                                                                                                      'Distance'] + self.graph[initial_node][neighbor]['weight']
-#                 else:
-#                     if self.graph.nodes[neighbor][person + 'Distance'] > self.graph.nodes[initial_node][person + 'Distance'] + self.graph[initial_node][neighbor]['weight']:
-#                         self.graph.nodes[neighbor][person + 'Distance'] = self.graph.nodes[initial_node][person +
-#                                                                                                          'Distance'] + self.graph[initial_node][neighbor]['weight']
-
-#         # Get the unvisited node with the lowest distance
-#         min_distance = 999
-#         min_node = ''
-#         for node in self.graph.nodes():
-#             if self.graph.nodes[node]['visited'] == False:
-#                 if self.graph.nodes[node][person + 'Distance'] < min_distance:
-#                     min_distance = self.graph.nodes[node][person + 'Distance']
-#                     min_node = node
-
-#         # If there's no unvisited node with a lower distance, stop the algorithm
-#         if min_node == '':
-#             return
-
-#         # If the destination node has been visited, stop the algorithm
-#         if self.graph.nodes[destination_node]['visited'] == True:
-#             return
-
-#         # Run the algorithm again with the node with the lowest distance as the initial node
-#         self.run_dijkstra(min_node, destination_node, person)
-
-#  # generate dijkstra algorithm for Javier and Andreina and return the shortest path for each one and resetting the graph for the next iteration
-#     def run_dijkstra(self, initial_node, destination_node, person):
-#         self.setup_dijkstra()
-#         current_node = initial_node
-#         while current_node != destination_node:
-#             for edge in self.graph.edges(current_node):
-#                 if self.graph.edges[edge]['visitedBy' + person] == False:
-#                     if self.graph.nodes[edge[1]]['visitedBy' + person] == False:
-#                         if self.graph.nodes[current_node]['visitedBy' + person] == False:
-#                             self.graph.nodes[current_node]['visitedBy' + person] = True
-#                         if self.graph.nodes[edge[1]]['visitedBy' + person] == False:
-#                             if self.graph.nodes[edge[1]][person + 'Distance'] > self.graph.nodes[current_node][person + 'Distance'] + self.graph.edges[edge]['weight']:
-#                                 self.graph.nodes[edge[1]][person + 'Distance'] = self.graph.nodes[current_node][person + 'Distance'] + self.graph.edges[edge]['weight']
-#                         self.graph.edges[edge]['visitedBy' + person] = True
-#             min_distance = 999
-#             for node in self.graph.nodes():
-#                 if self.graph.nodes[node]['visitedBy' + person] == False:
-#                     if self.graph.nodes[node][person + 'Distance'] < min_distance:
-#                         min_distance = self.graph.nodes[node][person + 'Distance']
-#                         current_node = node
-#         shortest_path = []
-#         current_node = destination_node
-#         while current_node != initial_node:
-#             for edge in self.graph.edges(current_node):
-#                 if self.graph.nodes[edge[1]][person + 'Distance'] + self.graph.edges[edge]['weight'] == self.graph.nodes[current_node][person + 'Distance']:
-#                     shortest_path.append(edge)
-#                     current_node = edge[1]
-#                     break
-#         shortest_path.reverse()
-#         return shortest_path
-
     def run_dijkstra(self, initial_node, destination_node, person):
         previous_node = {}
         non_visited_nodes = nx.Graph()
         non_visited_nodes.add_nodes_from(self.graph.nodes())
         non_visited_nodes.add_edges_from(self.graph.edges())
-        # print(non_visited_nodes)
         shortest_path = []
         distance = {}
 
@@ -196,12 +128,10 @@ class Graph:
                     minimum_node = node
 
             for edge in non_visited_nodes.edges(minimum_node):
-                # print(self.graph.nodes['visited'])
-                # if self.graph.nodes[edge[1]]['visitedBy' + person] == False and self.graph.nodes[edge[1]]['visitedByJavier'] == False:
                 if(self.graph.nodes[edge[1]] not in self.path):
                     if self.graph.nodes[edge[1]][person + 'Distance'] > self.graph.nodes[minimum_node][person + 'Distance'] + self.graph.edges[edge]['weight']:
                         self.graph.nodes[edge[1]][person + 'Distance'] = self.graph.nodes[minimum_node][person +
-                                                                                                        'Distance'] + self.graph.edges[edge]['weight']
+                                                                                                        'Distance'] + self.graph.edges[edge]['weight'] + (2 if(person == 'Andreina') else 0)
                         previous_node[edge[1]] = minimum_node
             non_visited_nodes.remove_node(minimum_node)
 
@@ -220,8 +150,13 @@ class Graph:
         shortest_path.insert(0, initial_node)
         distance['Distance'] = self.graph.nodes[destination_node][person + 'Distance']
         distance['Path'] = shortest_path
-        # self.graph.nodes['visited'] = shortest_path
         self.path = shortest_path
+        if person == "Javier": self.javier_path = shortest_path
+        if person == "Andreina" : self.andreina_path = shortest_path
+        for node in list(self.graph.nodes()):
+            if(node in shortest_path and node != destination_node):
+                # remove node from the graph if it is in the shortest path
+                self.graph.remove_node(node)
         return distance
 
 
